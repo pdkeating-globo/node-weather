95aaec83f9eebd4e11e2e59823fac10bc564a3d7
Patrick Keating <pkeating4@gmail.com>
2019-04-22 16:03:17 +0530

    Removed a console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('Running client-side javascript code')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
